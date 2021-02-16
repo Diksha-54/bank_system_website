a37f2f967b7288254f72e889c7c370701367d96a
Diksha-54 <67964082+Diksha-54@users.noreply.github.com>
2021-02-16 15:52:33 +0530

    Update url.py

--- user/url.py
+++ user/url.py
@@ -4,7 +4,7 @@ from . import views
 from django.conf.urls.static import *
 
 urlpatterns = [
-    path('index',views.index,name="index"),
+    path('',views.index,name="index"),
     path('viewall',views.viewall,name="viewall"),
     path('transfer',views.transfer,name="transfer"),
     path('transaction',views.transaction,name="transaction"),
